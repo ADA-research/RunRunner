7e0f9266d1cfdc06b6ad1c01a8a714865c267237
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-26 13:41:38 +0100

    Changes for Launch on Pip

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/thijssnelleman/RunRunner',
     author='Thijs Snelleman',
     author_email='fkt_sparkle@aim.rwth-aachen.de',
+    description='RunRunner is a wrapper library for creating and managing subprocesses and their status, mainly focussed on using Slurm but (in absence) can also work with local jobs.',
     packages=find_packages(include=['runrunner']),
     install_requires=[
         'pydantic <2.0a',
