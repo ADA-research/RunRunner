fee5d7c720c06e357795f034691c1a38597a238c
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-26 16:47:41 +0100

    Minor changes for documentation on Pypi

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     author='Thijs Snelleman',
     author_email='fkt_sparkle@aim.rwth-aachen.de',
     description='RunRunner is a wrapper library for creating and managing subprocesses and their status, mainly focussed on using Slurm but (in absence) can also work with local jobs.',
+    long_description=open("README.MD", 'r').read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(include=['runrunner']),
     install_requires=[
         'pydantic <2.0a',
