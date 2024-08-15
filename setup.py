4e03e944be39ad01db084104655fdf0f1d58d56b
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-08-15 12:38:32 +0200

    Updating version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name='RunRunner',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/thijssnelleman/RunRunner',
     author='Thijs Snelleman',
     author_email='fkt_sparkle@aim.rwth-aachen.de',
